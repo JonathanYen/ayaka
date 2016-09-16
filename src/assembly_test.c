90ccb9ad22d859114f4a652d93d7743733d37524
Yen Fan Che <ixxogux@gmail.com>
2016-09-16 15:16:03 +0800

    create src and include folder accordingly

---
+++ src/assembly_test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+   unsigned char A = 0;
+  // unsigned char* B = &A;
+
+   A = 1;
+   //printf("A = %d, B = %d\n", A, *B);
+
+   return 0;
+}
---
+++ src/linked_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+struct single_linked_list
+{
+   int data;
+   struct single_linked_list* next;
+};
+
+int main(void)
+{
+    struct single_linked_list list;
+    struct single_linked_list list2;
+    
+    list.next = &list2;
+
+    printf("list addr(%p), list.next addr(%p)\n",&list, list.next);
+    printf("list2 addr(%p)\n",&list2);
+    return 0;    
+}
